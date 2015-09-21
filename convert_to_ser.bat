9dfc5c99267ffdd538f5189b59e27cee89032cf7
Nikolay Mishin <mi@ya.ru>
2015-09-22 01:32:03 +0400

    добавил то, что прислала Наталия Андреевна Флоренская, моя бабушка

--- convert_to_ser.bat
+++ convert_to_ser.bat
@@ -1,2 +1,2 @@
-"c:\Users\TOSH\Documents\job\perl\strawberry-perl-5.20.0.1-32bit-PDL\perl\bin\perl.exe" convertFormat.pl mishin_family.xls.xlsx mishin_family.xls.ser
+"c:\Users\TOSH\Documents\job\perl\strawberry-perl-5.20.0.1-32bit-PDL\perl\bin\perl.exe" convertFormat.pl mishin_family.xls.xlsx mishin_family.xls.ser >> mishin_family.xls.log
 REM куь "c:\Dwimperl\perl\bin\perl.exe" .\scripts\convertFormat.pl .\export-BloodTree.xls ./export-BloodTree.ser
