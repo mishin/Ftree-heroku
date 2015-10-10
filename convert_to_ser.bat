7931833d98b22bc853ff40193519ee9cb1f81703
Nikolay Mishin <mi@ya.ru>
2015-10-11 03:46:14 +0400

    добавил Коростелевых

--- convert_to_ser.bat
+++ convert_to_ser.bat
@@ -1,2 +1,1 @@
 "c:\Users\TOSH\Documents\job\perl\strawberry-perl-5.20.0.1-32bit-PDL\perl\bin\perl.exe" convertFormat.pl mishin_family.xls.xlsx mishin_family.xls.ser > mishin_family.xls.log 2>&1
-REM куь "c:\Dwimperl\perl\bin\perl.exe" .\scripts\convertFormat.pl .\export-BloodTree.xls ./export-BloodTree.ser
